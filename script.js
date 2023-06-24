a6cef3cf42261589045b0e35a2c0685018a5f904
Blair Aitken <baitken@swin.edu.au>
2023-06-24 16:25:06 +1000

    Update

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ $('.input-field input, .input-field select').on('change', function() {
 
     if (sex && age && height && weight && brac) {
         const eliminationRate = 0.2; 
-        const drinkingDuration = (brac == 0.05) ? 10 : 15;
-        const expectedPeak = (brac == 0.05) ? 30 : 45;
+        const drinkingDuration = (brac == 0.08 || brac == 1) ? 15 : 10;
+        const expectedPeak = (brac == 0.08 || brac == 1) ? 45 : 35;
 
         let estimatedTotalBodyWater;
         if (sex == 'male') {
@@ -17,19 +17,26 @@ $('.input-field input, .input-field select').on('change', function() {
             estimatedTotalBodyWater = -2.097 + 0.1069 * height + 0.2466 * weight;
         }
 
-        const variable2E = (brac == 0.05) ? 0.55 : 0.88;
+        const variable2E = (brac == 0.08 || brac == 1) ? 0.88 : 0.55;
 
         const alcohol100DoseGram = (variable2E * estimatedTotalBodyWater / 0.8) + (eliminationRate * (drinkingDuration / 60 + expectedPeak / 60) * (estimatedTotalBodyWater / 0.8));
         const alcohol100DoseGramPerKg = alcohol100DoseGram / weight;
-    
+
         const alcohol40DoseGram = alcohol100DoseGram / 0.345;
         const alcohol40DoseGramPerKg = alcohol40DoseGram / weight;
+
+        let alcoholType;
+        if (brac == 1) {
+            alcoholType = "Stryyk:";
+        } else {
+            alcoholType = "Vodka:";
+        }
     
         const mixerDose = alcohol40DoseGram * 3;
 
         document.getElementById('result').innerHTML = `
-    <p>Alcohol dose : <strong>${alcohol40DoseGram.toFixed(2)} g</strong></p>
-    <p>Mixer dose: <strong>${mixerDose.toFixed(2)} g</strong></p>`;
+    <p1>${alcoholType}<br><strong>${alcohol40DoseGram.toFixed(2)} g</strong></p1>
+    <p2>Orange Juice:<br><strong>${mixerDose.toFixed(2)} g</strong></p2>`;
     }
 
 });
