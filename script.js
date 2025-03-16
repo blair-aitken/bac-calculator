c04329706e4cd01e298f4e56d4882348374ecea4
DESKTOP-H3FHK29\tyler <tyler.da55@gmail.com>
2025-03-16 20:23:55 +1100

    Minor ui tweaks and slight js refactor for simplicity

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
 function showContent(contentId) {
     // Hide all tabcontent elements
-    let tabcontents = document.querySelectorAll(".tabcontent");
-    tabcontents.forEach(function(content) {
-        content.classList.remove("active");
-    });
+    this.hideElements(document.querySelectorAll(".tabcontent"))
+
+    // Show the selected tabcontent
+    this.showElement(contentId);
 
     // Remove active class from all buttons
     let buttons = document.querySelectorAll(".sidebar button");
-    buttons.forEach(function(button) {
+    buttons.forEach(function (button) {
         button.classList.remove("active");
     });
-
-    // Show the selected tabcontent
-    document.getElementById(contentId).classList.add("active");
 
     // Add active class to the clicked button
     let clickedButton = document.querySelector(`.sidebar button[data-content="${contentId}"]`);
     clickedButton.classList.add("active");
 }
 
-function displayResults(alcoholDose, mixerDose){
+function displayResults(alcoholDose, mixerDose) {
+    this.showElement("calculator-results");
+    this.hideElement("calculator-instructions");
     document.getElementById("calculator-results").classList.remove("hidden");
     document.getElementById("calculator-instructions").classList.add("hidden");
-    
     document.getElementById("alcohol-dose").innerHTML = alcoholDose;
     document.getElementById("mixer-dose").innerHTML = mixerDose;
 }
 
-function displayInstructions(){
-    document.getElementById("calculator-results").classList.add("hidden");
-    document.getElementById("calculator-instructions").classList.remove("hidden");
+function displayInstructions() {
+    this.hideElement("calculator-results");
+    this.showElement("calculator-instructions");
+}
+
+// Hides all elements in a given NodeListOf<Element>
+function hideElements(elementList) {
+    elementList.forEach(function (content) {
+        content.classList.add("hidden");
+    });
+}
+
+// Shows all elements in a given NodeListOf<Element>
+function showElements(elementList) {
+    elementList.forEach(function (content) {
+        content.classList.remove("hidden");
+    });
+}
+
+// Hides an element by it's Id
+function hideElement(elementId) {
+    document.getElementById(elementId).classList.add("hidden");
+}
+
+// Shows an element by it's Id
+function showElement(elementId) {
+    document.getElementById(elementId).classList.remove("hidden");
 }
 
 
 // Initialize the first tabcontent and button as active
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     showContent('calculator');
 });
 
 
 // Function to calculate alcohol and mixer dose
-document.querySelectorAll('.input-field input, .input-field select').forEach(function(el) {
-    el.addEventListener('change', function() {
+document.querySelectorAll('.input-field input, .input-field select').forEach(function (el) {
+    el.addEventListener('change', function () {
         calculateDose();
     });
 });
@@ -97,14 +119,14 @@ function calculateDose() {
         const alcohol100DoseGram = (variable2E * estimatedTotalBodyWater / 0.8) + (eliminationRate * (drinkingDuration / 60 + expectedPeak / 60) * (estimatedTotalBodyWater / 0.8));
 
         const alcohol40DoseGram = alcohol100DoseGram / 0.345;
-        
+
         const alcohol40DoseGramg = alcohol40DoseGram / weight;
 
         const mixerDose = alcohol40DoseGram * 3;
 
         this.displayResults(alcohol40DoseGram.toFixed(2), mixerDose.toFixed(2));
     }
-    else{
+    else {
         this.displayInstructions();
     }
 }
@@ -114,7 +136,7 @@ function selectOption(field, value, button) {
 
     // Update the active state of buttons within the same group
     let buttons = button.parentElement.querySelectorAll('button');
-    buttons.forEach(function(btn) {
+    buttons.forEach(function (btn) {
         btn.classList.remove("active");
     });
 
